842fd5c294e70846e1fdab21c30416fc8abe5e62
mohammed anwr esaid <mohammedanwresaid@mohammeds-MacBook-Pro.local>
2022-01-20 15:58:52 +0300

    changed the icon button size

--- src/components/MaButton/MaButton.vue
+++ src/components/MaButton/MaButton.vue
@@ -10,7 +10,7 @@
   >
     <ma-icon :icon="icon" v-if="icon === 'spinner'" spin class="ma-btn-icon"/>
     <ma-icon :icon="icon" v-else-if="icon && icon !== 'spinner'" class="ma-btn-icon"/>
-    <ma-icon :icon="leftIcon" v-else-if="leftIcon" class="ma-left-btn"/>
+    <ma-icon :icon="leftIcon" size="xs" v-else-if="leftIcon" class="ma-left-btn"/>
     <slot/>
   </a-button>
 </template>
@@ -56,7 +56,7 @@ export default {
     },
     loading: { type: Boolean, default:false },
     disabled: { type: Boolean, default:false },
-    size: { type: String, default:'default' },
+    size: { type: String, default:'xs' },
   },
   computed: {
     classNames () {
