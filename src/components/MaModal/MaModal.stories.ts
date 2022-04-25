a1a46127c3cc4c7b89f8d2d6ff6d7e11e26cdca3
merveceylan <merve@mobileaction.co>
2022-04-25 10:37:55 +0300

    feat(2200) implement dialog

--- src/components/MaModal/MaModal.stories.ts
+++ src/components/MaModal/MaModal.stories.ts
@@ -41,7 +41,89 @@ export const Base = () => ({
               <p>Some contents...</p>
             </div>
           </ma-modal>
-          </div>`,
+          </div>
+        `,
 });
 
 Base.storyName = 'Base';
+
+export const CustomFooter = () => ({
+    components: { MaModal, MaButton },
+    data() {
+        return {
+            visible: false,
+        };
+    },
+    methods: {
+        openModal(){
+            this.visible = true;
+        },
+        close(){
+            this.visible = false;
+        },
+    },
+    template:
+        `
+          <div class="m-2">
+          <ma-button @click="openModal">Open Modal</ma-button>
+          <ma-modal 
+              :visible="visible" 
+              title="Basic Modal"
+              @ok="close"
+              @cancel="close"
+          >
+            <template #footer>
+              <ma-button key="back" @click="close">Return</ma-button>
+              <ma-button key="submit" type="primary" @click="close">Submit</ma-button>
+            </template>
+            <div>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+            </div>
+          </ma-modal>
+          </div>
+        `,
+});
+
+CustomFooter.storyName = 'Custom Footer';
+
+
+export const CustomHeader = () => ({
+    components: { MaModal, MaButton },
+    data() {
+        return {
+            visible: false,
+        };
+    },
+    methods: {
+        openModal(){
+            this.visible = true;
+        },
+        close(){
+            this.visible = false;
+        },
+    },
+    template:
+        `
+          <div class="m-2">
+          <ma-button @click="openModal">Open Modal</ma-button>
+          <ma-modal 
+              :visible="visible"
+              @ok="close"
+              @cancel="close"
+          >
+            <template #title>
+              <div>Example Header</div>
+            </template>
+            <div>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+            </div>
+          </ma-modal>
+          </div>
+        `,
+});
+
+CustomHeader.storyName = 'Custom Header';
