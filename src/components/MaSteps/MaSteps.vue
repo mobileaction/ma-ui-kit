ca3d657adeb67174d225e0dcf96c44cba07053ac
karahan6 <karahanmehmet6@gmail.com>
2022-07-27 08:26:56 +0300

    feat(DUV-2448) ma-steps

---
+++ src/components/MaSteps/MaSteps.vue
@@ -0,0 +1,50 @@
+<template>
+    <a-steps
+        class="ma-steps"
+        :current="data"
+        :direction="direction"
+        @change="changeStep"
+    >
+        <slot/>
+    </a-steps>
+</template>
+<script>
+    import Steps from 'ant-design-vue/lib/steps/index.js';
+
+    export default {
+        name: 'ma-steps',
+        components: {
+            ASteps: Steps,
+        },
+        props: {
+            value: { type: Number, default: 0 },
+            direction: { type: String, default: 'horizontal' },
+        },
+        data() {
+            return {
+                data: this.value,
+            };
+        },
+        methods: {
+            changeStep(data) {
+                this.$emit('input', data);
+            },
+        },
+        watch: {
+            value(val) {
+                this.data = val;
+            },
+        },
+    };
+</script>
+
+<style lang="scss" scoped>
+@import '~ant-design-vue/lib/steps/style/index.css';
+.ma-steps {
+  ::v-deep.ant-steps-icon {
+    .anticon {
+      vertical-align: text-bottom;
+    }
+  }
+}
+</style>
