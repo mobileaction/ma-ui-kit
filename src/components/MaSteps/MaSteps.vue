c1def1f306855bc7d426b155895a7c074fe7bb59
karahan6 <karahanmehmet6@gmail.com>
2022-07-28 14:59:19 +0300

    feat (DUV-2448) style edit

--- src/components/MaSteps/MaSteps.vue
+++ src/components/MaSteps/MaSteps.vue
@@ -42,12 +42,46 @@
 
 <style lang="scss" scoped>
 @import '~ant-design-vue/lib/steps/style/index.css';
+
 .ma-steps {
   width: auto;
-  ::v-deep.ant-steps-icon {
-    .anticon {
-      vertical-align: text-bottom;
+  ::v-deep.ant-steps-item-active{
+    .ant-steps-item-container{
+      .ant-steps-item-content > .ant-steps-item-title::after{
+
+      }
+      .ant-steps-item-icon {
+          background-color: #1dcfb4;
+          border-color: #1dcfb4;
+          .ant-steps-icon {
+            .anticon {
+              vertical-align: text-bottom;
+            }
+          }
+        }
     }
   }
+  ::v-deep.ant-steps-item-finish{
+    .ant-steps-item-container{
+      .ant-steps-item-content > .ant-steps-item-title::after{
+        background-color: #32355c;
+      }
+      .ant-steps-item-icon {
+        background-color: #32355c;
+        border-color: #32355c;
+        &:hover{
+          background-color: #32355c;
+          border-color: #32355c;
+        }
+        .ant-steps-icon {
+          .anticon {
+            vertical-align: text-bottom;
+            color: #fff;
+          }
+        }
+      }
+    }
+  }
+
 }
 </style>
